ec1a1db8fa462196741f21b91ab53abce0e7e39b
Andrew Chiang <andrew.lc.chiang@gmail.com>
2015-08-24 21:55:53 +0800

    Move query track record task from onCreateView() to a AsyncTask, and retain views of MediaPlayerDialogFragment during configuration changing

--- app/src/main/java/com/example/android/spotifystreamer/music/MediaPlayerDialogFragment.java
+++ app/src/main/java/com/example/android/spotifystreamer/music/MediaPlayerDialogFragment.java
@@ -1,6 +1,7 @@
 package com.example.android.spotifystreamer.music;
 
 import android.database.Cursor;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.DialogFragment;
@@ -27,6 +28,7 @@ public class MediaPlayerDialogFragment extends DialogFragment {
 
     private static final String LOG_TAG = MediaPlayerDialogFragment.class.getSimpleName();
     private static final String PLAYER_TAG = "PLAYER_TAG";
+    private static PlayerInfo mPlayerInfoForRestore;
 
     private TextView mArtistTextView;
     private TextView mAlbumTextView;
@@ -39,63 +41,147 @@ public class MediaPlayerDialogFragment extends DialogFragment {
     private ImageButton mPlayPauseIamgeButton;
     private ImageButton mNextImageButton;
 
-//    @Override
-//    public void onSaveInstanceState(Bundle outState) {
-//        super.onSaveInstanceState(outState);
-//
-//        PlayerInfo playerInfo = new PlayerInfo(
-//                mArtistTextView.getText(),
-//                mAlbumTextView.getText(),
-//                mTrackTextView.getText(),
-//                mArtworkImageView.
-//
-//        );
-//        outState.putParcelable(PLAYER_TAG, playerInfo);
-//    }
-//
-//    @Override
-//    public void onViewStateRestored(@Nullable Bundle savedInstanceState) {
-//        super.onViewStateRestored(savedInstanceState);
-//
-//        if(savedInstanceState != null){
-//            PlayerInfo playerInfo = savedInstanceState.getParcelable(PLAYER_TAG);
-//            if(playerInfo != null) {
-//                mArtistTextView.setText(playerInfo.getArtist());
-//                mAlbumTextView.setText(playerInfo.getAlbum());
-//                mTrackTextView.setText(playerInfo.getTrack());
-//
-//                String imageUrl = playerInfo.getImage();
-//                if (!imageUrl.isEmpty()) {
-//                    Picasso.with(getActivity())
-//                            .load(imageUrl)
-//                            .resizeDimen(R.dimen.player_artwork, R.dimen.player_artwork)
-//                            .centerCrop()
-//                            .into(mArtworkImageView);
-//                }
-//
-//                mEndTextView.setText(playerInfo.getDuration());
-//                // SeekBar
-//                mPreviousImageButton.setEnabled(playerInfo.getToPrev());
-//
-//                if (playerInfo.getPlaying()) {
-//                    mPlayPauseIamgeButton.setImageResource(android.R.drawable.ic_media_pause);
-//                } else {
-//                    mPlayPauseIamgeButton.setImageResource(android.R.drawable.ic_media_play);
-//                }
-//
-//                mNextImageButton.setEnabled(playerInfo.getToNext());
-//            }
-//        }
-//    }
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        Log.d(LOG_TAG, "in onSaveInstanceState");
+        outState.putParcelable(PLAYER_TAG, mPlayerInfoForRestore);
+    }
+
+    @Override
+    public void onViewStateRestored(@Nullable Bundle savedInstanceState) {
+        super.onViewStateRestored(savedInstanceState);
+
+        Log.d(LOG_TAG, "in onViewStateRestored");
+
+        if(savedInstanceState != null){
+            PlayerInfo playerInfo = savedInstanceState.getParcelable(PLAYER_TAG);
+            updatePlayer(playerInfo);
+        }
+    }
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.track_player, container, false);
+        Log.d(LOG_TAG, "in onCreateView");
+
+        mArtistTextView = (TextView) rootView.findViewById(R.id.artist_name_textview);
+        mAlbumTextView = (TextView) rootView.findViewById(R.id.album_name_textview);
+        mTrackTextView = (TextView) rootView.findViewById(R.id.track_name_textview);
+        mArtworkImageView = (ImageView) rootView.findViewById(R.id.album_artwork_imageview);
 
         if(savedInstanceState == null) {
+
             Bundle args = getArguments();
             long trackId = args.getLong("trackId");
+            QueryTrackFromDbTask queryTrackFromDbTask = new QueryTrackFromDbTask();
+            queryTrackFromDbTask.execute(trackId);
+
+//            Cursor cursor = getActivity().getContentResolver().query(
+//                    TopTracksContract.TopTracksEntry.buildTrackUri(trackId),
+//                    null,
+//                    null,
+//                    null,
+//                    null
+//            );
+//
+//            if (cursor != null) {
+//                Log.d(LOG_TAG, "cursor is not null");
+//                if (cursor.moveToFirst()) {
+//                    Log.d(LOG_TAG, "cursor is moving the first record");
+//
+//                    String artist = cursor.getString(cursor.getColumnIndex(TopTracksContract.TopTracksEntry.COLUMN_ARTIST));
+//                    String album = cursor.getString(cursor.getColumnIndex(TopTracksContract.TopTracksEntry.COLUMN_ALBUM));
+//                    String track = cursor.getString(cursor.getColumnIndex(TopTracksContract.TopTracksEntry.COLUMN_TRACK));
+//                    String small_artwork = cursor.getString(cursor.getColumnIndex(TopTracksContract.TopTracksEntry.COLUMN_SMALL_IMAGE));
+//                    String large_artwork = cursor.getString(cursor.getColumnIndex(TopTracksContract.TopTracksEntry.COLUMN_LARGE_IMAGE));
+//                    String preview_url = cursor.getString(cursor.getColumnIndex(TopTracksContract.TopTracksEntry.COLUMN_PREVIEW_URL));
+//
+//                    mArtistTextView = (TextView) rootView.findViewById(R.id.artist_name_textview);
+//                    mArtistTextView.setText(artist);
+//
+//                    mAlbumTextView = (TextView) rootView.findViewById(R.id.album_name_textview);
+//                    mAlbumTextView.setText(album);
+//
+//                    mTrackTextView = (TextView) rootView.findViewById(R.id.track_name_textview);
+//                    mTrackTextView.setText(track);
+//
+//                    mArtworkImageView = (ImageView) rootView.findViewById(R.id.album_artwork_imageview);
+//                    if (!large_artwork.isEmpty()) {
+//                        Picasso.with(getActivity())
+//                                .load(large_artwork)
+//                                .resizeDimen(R.dimen.player_artwork, R.dimen.player_artwork)
+//                                .centerCrop()
+//                                .into(mArtworkImageView);
+//                    }
+//
+//
+//                    mSeekBar = (SeekBar) rootView.findViewById(R.id.duration_seekbar);
+//                    mStartTextView = (TextView) rootView.findViewById(R.id.duration_start_textview);
+//                    mEndTextView = (TextView) rootView.findViewById(R.id.duration_end_textview);
+//                    mPreviousImageButton = (ImageButton) rootView.findViewById(R.id.previous_imagebutton);
+//                    mPlayPauseIamgeButton = (ImageButton) rootView.findViewById(R.id.play_pause_imagebutton);
+//
+////                mPlayPauseIamgeButton.setOnClickListener(new View.OnClickListener() {
+////                    @Override
+////                    public void onClick(View v) {
+////                        if (mMusicService != null) {
+////                            mMusicService.playTrack(previewUrl);
+////                        }
+////                    }
+////                });
+//
+//                    mNextImageButton = (ImageButton) rootView.findViewById(R.id.next_imagebutton);
+//                } else {
+//                    Log.d(LOG_TAG, "cursor is not moving to the first record.");
+//                }
+//
+//                cursor.close();
+//            } else {
+//                Utils.displayToast(getActivity(),
+//                        R.string.error_track_is_not_available,
+//                        Toast.LENGTH_SHORT,
+//                        Gravity.CENTER);
+//            }
+
+        }
+        return rootView;
+    }
+
+    private void updatePlayer(PlayerInfo playerInfo){
+        if(playerInfo != null){
+
+            // Refers to the instance of PlayerInfo.
+            mPlayerInfoForRestore = playerInfo;
+
+            mArtistTextView.setText(playerInfo.getArtist());
+            mAlbumTextView.setText(playerInfo.getAlbum());
+            mTrackTextView.setText(playerInfo.getTrack());
+            String artwork = playerInfo.getImage();
+            if (!artwork.isEmpty()) {
+                Picasso.with(getActivity())
+                        .load(artwork)
+                        .resizeDimen(R.dimen.player_artwork, R.dimen.player_artwork)
+                        .centerCrop()
+                        .into(mArtworkImageView);
+            }
+        }
+    }
+
+    public class QueryTrackFromDbTask extends AsyncTask<Long, Void, PlayerInfo>{
+        @Override
+        protected PlayerInfo doInBackground(Long... params) {
+            long trackId = params[0];
+            String artist;
+            String album;
+            String track;
+            String small_artwork;
+            String large_artwork;
+            String preview_url;
+
+            PlayerInfo playerInfo = null;
 
             Cursor cursor = getActivity().getContentResolver().query(
                     TopTracksContract.TopTracksEntry.buildTrackUri(trackId),
@@ -110,61 +196,45 @@ public class MediaPlayerDialogFragment extends DialogFragment {
                 if (cursor.moveToFirst()) {
                     Log.d(LOG_TAG, "cursor is moving the first record");
 
-                    String artist = cursor.getString(cursor.getColumnIndex(TopTracksContract.TopTracksEntry.COLUMN_ARTIST));
-                    String album = cursor.getString(cursor.getColumnIndex(TopTracksContract.TopTracksEntry.COLUMN_ALBUM));
-                    String track = cursor.getString(cursor.getColumnIndex(TopTracksContract.TopTracksEntry.COLUMN_TRACK));
-                    String small_artwork = cursor.getString(cursor.getColumnIndex(TopTracksContract.TopTracksEntry.COLUMN_SMALL_IMAGE));
-                    String large_artwork = cursor.getString(cursor.getColumnIndex(TopTracksContract.TopTracksEntry.COLUMN_LARGE_IMAGE));
-                    String preview_url = cursor.getString(cursor.getColumnIndex(TopTracksContract.TopTracksEntry.COLUMN_PREVIEW_URL));
+                    artist = cursor.getString(cursor.getColumnIndex(TopTracksContract.TopTracksEntry.COLUMN_ARTIST));
+                    album = cursor.getString(cursor.getColumnIndex(TopTracksContract.TopTracksEntry.COLUMN_ALBUM));
+                    track = cursor.getString(cursor.getColumnIndex(TopTracksContract.TopTracksEntry.COLUMN_TRACK));
+                    small_artwork = cursor.getString(cursor.getColumnIndex(TopTracksContract.TopTracksEntry.COLUMN_SMALL_IMAGE));
+                    large_artwork = cursor.getString(cursor.getColumnIndex(TopTracksContract.TopTracksEntry.COLUMN_LARGE_IMAGE));
+                    preview_url = cursor.getString(cursor.getColumnIndex(TopTracksContract.TopTracksEntry.COLUMN_PREVIEW_URL));
 
-                    mArtistTextView = (TextView) rootView.findViewById(R.id.artist_name_textview);
-                    mArtistTextView.setText(artist);
+                    playerInfo = new PlayerInfo(
+                            artist,
+                            album,
+                            track,
+                            large_artwork,
+                            "",
+                            "",
+                            false,
+                            false,
+                            false
+                    );
 
-                    mAlbumTextView = (TextView) rootView.findViewById(R.id.album_name_textview);
-                    mAlbumTextView.setText(album);
-
-                    mTrackTextView = (TextView) rootView.findViewById(R.id.track_name_textview);
-                    mTrackTextView.setText(track);
-
-                    mArtworkImageView = (ImageView) rootView.findViewById(R.id.album_artwork_imageview);
-                    if (!large_artwork.isEmpty()) {
-                        Picasso.with(getActivity())
-                                .load(large_artwork)
-                                .resizeDimen(R.dimen.player_artwork, R.dimen.player_artwork)
-                                .centerCrop()
-                                .into(mArtworkImageView);
-                    }
-
-
-                    mSeekBar = (SeekBar) rootView.findViewById(R.id.duration_seekbar);
-                    mStartTextView = (TextView) rootView.findViewById(R.id.duration_start_textview);
-                    mEndTextView = (TextView) rootView.findViewById(R.id.duration_end_textview);
-                    mPreviousImageButton = (ImageButton) rootView.findViewById(R.id.previous_imagebutton);
-                    mPlayPauseIamgeButton = (ImageButton) rootView.findViewById(R.id.play_pause_imagebutton);
-
-//                mPlayPauseIamgeButton.setOnClickListener(new View.OnClickListener() {
-//                    @Override
-//                    public void onClick(View v) {
-//                        if (mMusicService != null) {
-//                            mMusicService.playTrack(previewUrl);
-//                        }
-//                    }
-//                });
-
-                    mNextImageButton = (ImageButton) rootView.findViewById(R.id.next_imagebutton);
                 } else {
                     Log.d(LOG_TAG, "cursor is not moving to the first record.");
                 }
 
                 cursor.close();
             } else {
-                Utils.displayToast(getActivity(),
+                Utils.displayToast(
+                        getActivity(),
                         R.string.error_track_is_not_available,
                         Toast.LENGTH_SHORT,
-                        Gravity.CENTER);
+                        Gravity.CENTER
+                );
             }
 
+            return playerInfo;
         }
-        return rootView;
+
+        @Override
+        protected void onPostExecute(PlayerInfo playerInfo) {
+            updatePlayer(playerInfo);
+        }
     }
 }
